fadf7cc9ee960275c44651cb347d628871482f06
Carlos Jacquin Venencia <atuxlife@gmail.com>
2022-07-25 13:56:55 -0500

    Finalización primera versión de Auth

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { UsersService } from '../users/users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/me')
+  me(@Req() request) {
+    const userId = request.user.userId;
+    return this.userService.findOne(userId);
+  }
+}
